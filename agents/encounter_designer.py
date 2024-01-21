ef05cfdf6d3f32725cc2d3502e39c7e66d1a478c
Anthus Williams <anthuswilliams@gmail.com>
2024-01-21 19:14:57 +0000

    [wip] difficulty check

--- agents/encounter_designer.py
+++ agents/encounter_designer.py
@@ -1,5 +1,11 @@
 import json
-import simpleaichat
+
+from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser
+from langchain_community.utils.openai_functions import convert_pydantic_to_openai_function
+from langchain.chat_models import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
+from langchain_core.messages import SystemMessage
 
 from pydantic import BaseModel, Field
 from typing import List
@@ -28,52 +34,63 @@ class Enemy(BaseModel):
     name: str = Field("The name of the creature or humanoid enemy")
     stats: StatBlock = Field("""The stat block for the enemy""")
     cr: str = Field("The enemy's Challenge Rating")
+    xp: int = Field("The XP value of the enemy")
     actions: List[Action] = Field("A list of actions that the enemy may take")
     other_info: str = Field("Any other information relevant to how the enemy appears, acts, moves, or behaves")
+
+class Player(BaseModel):
+    """Defines a player character who will participate in the encounter"""
+    level: int = Field("The player's level")
 
 class Encounter(BaseModel):
     """Defines an encounter including any enemies, terrain, setting, etc."""
     enemies: List[Enemy] = Field("List of enemies included in the encounter")
+#    players: List[Player] = Field("List of players included in the encounter")
+#    difficulty: str = Field("The difficulty level of the encounter")
 
 
 system_message = """
-    You are designing an encounter for Dungeons and Dragons 5th Edition.  You have the following information:
-    Input Parameters:
+You are designing an encounter for Dungeons and Dragons 5th Edition.  You have the following information:
+Input Parameters:
 
-    Player Information: Number of players and their respective levels.
-    Desired Difficulty Level: Easy, Medium, Hard, or Deadly.
-    Narrative and Setting Context: Brief description of the current narrative and setting in which the encounter will take place.
-    
-    As the encounter designer you should consider the following:
+Player Information: Number of players and their respective levels.
+Desired Difficulty Level: Easy, Medium, Hard, or Deadly.
+Narrative and Setting Context: Brief description of the current narrative and setting in which the encounter will take place.
 
-    Select Appropriate Creatures: Choose creatures with Challenge Ratings (CR) that match the desired difficulty level, ensuring that the total encounter difficulty is appropriate for the players' strength.
-    Encounter Design Principles:
-    Balance: Ensure that the encounter matches the desired difficulty level.
-    Narrative Integration: Design the encounter to fit seamlessly into the ongoing story and setting.
-    Output of encounter details:
-    Provide a list of creatures or adversaries, including their CRs.
+As the encounter designer you should consider the following:
+
+Select Appropriate Creatures: Choose creatures with Challenge Ratings (CR) that match the desired difficulty level, ensuring that the total encounter difficulty is appropriate for the players' strength.
+Encounter Design Principles:
+Balance: Ensure that the encounter matches the desired difficulty level.
+Narrative Integration: Design the encounter to fit seamlessly into the ongoing story and setting.
+Output of encounter details:
+Provide a list of creatures or adversaries, including their CRs and XP.
+"""
+
+input_message = """
+Player Information: {players}
+Desired Difficulty Level: {difficulty}
+Narrative and Setting Context: {setting}
 """
 
 def design_encounter(instructions):        
-    ai = simpleaichat.AIChat(console=False,
-        save_messages=False,
-        model="gpt-3.5-turbo",
-        params={"temperature": 0.0},
-        system=system_message
+    model = ChatOpenAI(temperature=0)
+    parser = JsonOutputFunctionsParser()
+
+    chain = (
+        ChatPromptTemplate.from_messages([
+            SystemMessage(content=system_message),
+            HumanMessagePromptTemplate.from_template(input_message)
+        ])
+        | model.bind(functions=[convert_pydantic_to_openai_function(Encounter)])
+        | parser
+       # | RunnablePassthrough.assign(xp_values=lambda r: [e["xp"] for e in r["enemies"]])
     )
 
-    response_structured = ai(
-        instructions,
-        output_schema=Encounter
-    )
-
-    return response_structured
+    return chain.invoke({"players": "3 level 1 players", "difficulty": "medium", "setting": "temple"})
 
 
 if __name__ == "__main__":
     encounter = design_encounter("Design an encounter for three level 1 players that is medium difficulty, and is set in a temple.")
-    for e in encounter["enemies"]:
-        camelized_name = e["name"].replace(" ", "_").lower()
-        with open(f"benchmarks/enemies/{camelized_name}.json", "w") as fh:
-            fh.write(json.dumps(e, indent=4))
-    print(encounter)
+    
+    print(json.dumps(encounter, indent=4))
