011a92c716a50c2bfe1425c6af51062cdb294f58
Anthus Williams <anthuswilliams@gmail.com>
2024-12-28 21:06:40 +0000

    refactor adjudicator agent into module

--- ingest/attach_and_embed.py
+++ ingest/attach_and_embed.py
@@ -1,26 +1,8 @@
 import requests
-import json
 import os
 import base64
 
-
-ELASTIC_HOST = "https://192.168.1.153:9200"
-
-
-def elastic_request(data=None, method=None, url=None):
-    if method == None:
-        method = requests.get
-    if data:
-        data = json.dumps(data)
-
-    return method(f"{ELASTIC_HOST}/{url}",
-                  headers={
-                      "Content-Type": "application/json",
-                      "Accept": "application/json",
-                      "Authorization": f"ApiKey {os.getenv('ELASTIC_API_KEY')}"
-                  },
-                  verify=False,
-                  data=data)
+from utils.elastic import elastic_request
 
 
 def create_inference_endpoint():
