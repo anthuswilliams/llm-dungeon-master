f453c0ac9cb8b5278b1876f8f3b3b31b1002bbe0
Anthus Williams <anthuswilliams@gmail.com>
2025-01-02 01:59:59 +0000

    fix html location

--- app/api.py
+++ app/api.py
@@ -1,17 +1,21 @@
+from typing import List
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
 
 app = FastAPI()
 
-from typing import List
 
 class Messages(BaseModel):
     messages: List[str]
 
+
 @app.get("/")
 async def read_index():
-    with open("app/frontend/src/public/index.html", "r") as file:
+    with open("app/frontend/public/index.html", "r") as file:
         content = file.read()
     return Response(content, media_type="text/html")
+
+
+@app.post("/messages")
 async def create_message(messages: Messages):
     return {"messages": messages.messages}
